4bbc5f4720d55891c6298a3aeed26cacc07dd5ba
LilBao <nnvb1311@gmail.com>
2023-11-10 21:05:59 +0700

    fix wishlist

--- src/main/java/com/rhymthwave/Service/AccountService.java
+++ src/main/java/com/rhymthwave/Service/AccountService.java
@@ -5,8 +5,7 @@ import com.rhymthwave.entity.Account;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface AccountService {
-
-
 	Account findAdminByEmail(HttpServletRequest request);
 	
+	Account findAccountByUsername(String username);
 }
